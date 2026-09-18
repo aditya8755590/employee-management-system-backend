c9f1f05e82be12c5546a05281ded3bfd2f398c21
Aditya Kumar <aditya.kumar875590@gmail.com>
2026-09-18 18:06:45 +0530

    refactor: remove unused HttpStatusCode import from controller

--- src/main/java/com/harsh/employee/controller/EmployeeController.java
+++ src/main/java/com/harsh/employee/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import com.harsh.employee.model.Employee;
 import com.harsh.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
