3faaea0e7cbd4bdd3efd9f6eb8fa00f025f3e822
Aditya Kumar <aditya.kumar875590@gmail.com>
2026-09-18 18:30:57 +0530

    docs: document the repository interface with Javadoc

--- src/main/java/com/harsh/employee/repository/EmployeeRepository.java
+++ src/main/java/com/harsh/employee/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.harsh.employee.repository;
 import com.harsh.employee.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Data access layer for {@link EmployeeEntity} records.
+ */
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
 }
